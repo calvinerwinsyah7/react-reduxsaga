798a9d101227441d07b19cda1dcb7d29bb20c543
Calvin Erwinsyah <104066655+calvinerwinsyah7@users.noreply.github.com>
2023-09-08 23:16:27 +0700

    [ReduxSaga-Template] Fix TSConfig and ESLint errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import React from "react"
 import { BrowserRouter as Router, Routes } from "react-router-dom"
 
 import routeData from "./routes"
--- src/utils/routeGenerator.tsx
+++ src/utils/routeGenerator.tsx
@@ -1,9 +1,10 @@
+import React from "react"
 import { Route } from "react-router-dom"
 
 export interface RouteData {
-  path: string;
-  component: () => JSX.Element;
-  name: string; // Add any additional route-specific properties
+  path: string
+  component: () => JSX.Element
+  name: string // Add any additional route-specific properties
 }
 
 const generateRoutes = (routeData: RouteData[]) => {
